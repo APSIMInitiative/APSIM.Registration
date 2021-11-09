45005e89411a600a95c5cb4d4708a7fb549c3e91
Drew Holzworth <drew.holzworth@csiro.au>
2021-11-09 12:34:24 +1000

    Add migrator project for historical purposes

---
+++ APSIM.Registration.Migration/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace APSIM.Registration.Migration
+{
+    class Program
+    {
+        private const string oldConnectionStringVar = "OLD_CONN_STRING";
+        private const string newConnectionStringVar = "NEW_CONN_STRING";
+
+        static void Main(string[] args)
+        {
+            string oldConnectionString = Environment.GetEnvironmentVariable(oldConnectionStringVar);
+            if (string.IsNullOrEmpty(oldConnectionString))
+                throw new Exception($"Old connection string not set");
+
+            string newConnectionString = Environment.GetEnvironmentVariable(newConnectionStringVar);
+            if (string.IsNullOrEmpty(newConnectionString))
+                throw new Exception($"New connection string not set");
+            Migrator migrator = new Migrator(oldConnectionString, newConnectionString);
+            ushort num = args.Length > 0 ? ushort.Parse(args[0]) : (ushort)0;
+            migrator.Migrate(num);
+            Console.WriteLine("Migration Successful.");
+        }
+    }
+}

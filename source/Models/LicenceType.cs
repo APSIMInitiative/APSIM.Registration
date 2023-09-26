4d462c3fdc861974b7dd6cf68ddb5064dd777a53
Julian Rich <123442863+ric394@users.noreply.github.com>
2023-09-26 16:37:49 +1000

    Changes to accomodate licence changes

--- source/Models/LicenceType.cs
+++ source/Models/LicenceType.cs
@@ -1,14 +1,13 @@
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace APSIM.Registration.Models
 {
     public enum LicenceType
     {
-        [Display(Name = "Non-Commercial")]
-        NonCommercial,
+        [Display(Name = "General Use")]
+        GeneralUse,
 
-        [Display(Name = "Commercial")]
-        Commercial
+        [Display(Name = "Special Use")]
+        SpecialUse
     }
 }
